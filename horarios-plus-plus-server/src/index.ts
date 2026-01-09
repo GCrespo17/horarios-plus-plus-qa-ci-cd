242c085e65cef098b2d67fe5154e8dc76c224623
Gabriel <gcrespo1705@gmail.com>
2026-01-08 23:23:35 -0400

    created index.ts to build empty project

---
+++ horarios-plus-plus-server/src/index.ts
@@ -0,0 +1,14 @@
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Horarios++ Server")
+  .get("/health", () => ({ status: "ok" }))
+  .listen(process.env.PORT || 3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+export type App = typeof app;
